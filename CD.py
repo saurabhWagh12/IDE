17ac0b301bfe0350ca82ace06d199cccc78fa135
Saurabh Wagh <saurabhwagh.sw.12@gmail.com>
2024-03-10 11:46:02 +0530

    Added Terminal Feature

--- CD.py
+++ CD.py
@@ -12,13 +12,16 @@ def setFilePath(path):
     global filepath
     filepath = path
 
+def run_command(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, shell=True, universal_newlines=True)
+    output, error = process.communicate()
+    update_output(output, error)
+
 def run_python():
     command = f'python3 {filepath}'
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, universal_newlines=True)
     output, error = process.communicate()
-    codeOutput.delete('1.0', END)
-    codeOutput.insert('1.0', output.decode("utf-8"))
-    codeOutput.insert('1.0', error.decode("utf-8"))
+    update_output(output, error)
 
 def run_java():
     code = editor.get('1.0', END)
@@ -34,16 +37,16 @@ def run_java():
     # Compile and run the Java code using subprocess
     compile_process = subprocess.run(['javac', file_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=os.path.dirname(filepath))
     if compile_process.returncode == 0:
-        run_process = subprocess.Popen(['java', file_name[:-5]], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=os.path.dirname(filepath))
+        run_process = subprocess.Popen(['java', file_name[:-5]], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=os.path.dirname(filepath), universal_newlines=True)
         output, error = run_process.communicate()
-        codeOutput.delete('1.0', END)
-        codeOutput.insert('1.0', output.decode("utf-8"))
-        codeOutput.insert('1.0', error.decode("utf-8"))
+        update_output(output, error)
     else:
-        codeOutput.delete('1.0', END)
-        codeOutput.insert('1.0', compile_process.stderr.decode("utf-8"))
+        update_output(compile_process.stderr.decode("utf-8"))
 
-
+def update_output(output, error=""):
+    codeOutput.delete('1.0', END)
+    codeOutput.insert('1.0', output)
+    codeOutput.insert('1.0', error)
 
 def saveAs():
     if filepath == '':
@@ -63,6 +66,13 @@ def openFile():
         editor.insert('1.0', code)
         setFilePath(path)
 
+def run_enter(event):
+    command = codeOutput.get("1.0", "end-1c")  
+    run_command(command)
+
+def clear_terminal():
+    codeOutput.delete('1.0', END)
+
 menuBar = Menu(compiler)
 
 fileMenu = Menu(menuBar, tearoff=0)
@@ -75,24 +85,25 @@ menuBar.add_cascade(label='File', menu=fileMenu)
 runMenu = Menu(menuBar, tearoff=0)
 runMenu.add_command(label='Run Python', command=run_python)
 runMenu.add_command(label='Run Java', command=run_java)
+runMenu.add_command(label='Clear Terminal', command=clear_terminal)
 menuBar.add_cascade(label='Run', menu=runMenu)
 
 compiler.config(menu=menuBar)
 
 editor = Text(compiler)
-editor.grid(row=0, column=0, sticky="nsew")  
-compiler.grid_rowconfigure(0, weight=1)       
-compiler.grid_columnconfigure(0, weight=1)    
+editor.grid(row=0, column=0, sticky="nsew")
+compiler.grid_rowconfigure(0, weight=1)
+compiler.grid_columnconfigure(0, weight=1)
 
 def on_tab_pressed(event):
-    editor.insert(INSERT, "    ")  
-    return 'break'  # Prevents the default behavior of the Tab key
+    editor.insert(INSERT, "    ")
+    return 'break'
 
-# Tab button Updation
 editor.bind("<Tab>", on_tab_pressed)
 
-codeOutput = Text(compiler, height=10)
-codeOutput.grid(row=1, column=0, sticky="nsew")  
-compiler.grid_rowconfigure(1, weight=1)          
+codeOutput = Text(compiler, height=8)
+codeOutput.grid(row=1, column=0, sticky="nsew")
+compiler.grid_rowconfigure(1, weight=1)
+codeOutput.bind("<Return>", run_enter)  
 
 compiler.mainloop()
